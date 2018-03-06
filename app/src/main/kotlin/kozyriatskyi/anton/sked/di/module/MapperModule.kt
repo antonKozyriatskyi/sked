a1c669ee90b6699a2e6c25bd151eeef05200d245
Anton Kozyriatskyi <dev.anton.kozyriatskyi@gmail.com>
2018-03-06 19:25:50 +0200

    Move providing DateFormatter to MapperModule; Make DayMapper and LessonMapper scoped singletons

--- app/src/main/kotlin/kozyriatskyi/anton/sked/di/module/MapperModule.kt
+++ app/src/main/kotlin/kozyriatskyi/anton/sked/di/module/MapperModule.kt
@@ -6,14 +6,21 @@ import kozyriatskyi.anton.sked.data.pojo.DayMapper
 import kozyriatskyi.anton.sked.data.pojo.LessonMapper
 import kozyriatskyi.anton.sked.data.repository.DateFormatter
 import kozyriatskyi.anton.sked.data.repository.ResourceManager
+import kozyriatskyi.anton.sked.di.App
 
 @Module
 class MapperModule {
 
+    @App
     @Provides
     fun provideLessonMapper(resourceManager: ResourceManager): LessonMapper = LessonMapper(resourceManager)
 
+    @App
     @Provides
     fun provideDayMapper(lessonMapper: LessonMapper, dateFormatter: DateFormatter): DayMapper =
             DayMapper(lessonMapper, dateFormatter)
+
+    @App
+    @Provides
+    fun provideDateFormatter(resourceManager: ResourceManager): DateFormatter = DateFormatter(resourceManager)
 }
