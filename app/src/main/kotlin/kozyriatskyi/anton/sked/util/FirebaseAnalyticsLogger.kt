9622a4c3876252ee939bac2d710a484d98d6c521
Anton Kozyriatskyi <dev.anton.kozyriatskyi@gmail.com>
2018-03-09 18:50:27 +0200

    Rename FirebaseLogger => FirebaseAnalyticsLogger

--- app/src/main/kotlin/kozyriatskyi/anton/sked/util/FirebaseAnalyticsLogger.kt
+++ app/src/main/kotlin/kozyriatskyi/anton/sked/util/FirebaseAnalyticsLogger.kt
@@ -3,7 +3,7 @@ package kozyriatskyi.anton.sked.util
 import android.content.Context
 import com.google.firebase.analytics.FirebaseAnalytics
 
-class FirebaseLogger(private val context: Context) {
+class FirebaseAnalyticsLogger(private val context: Context) {
 
     fun logStudent() {
         FirebaseAnalytics.getInstance(context).setUserProperty("user_type", "student")
