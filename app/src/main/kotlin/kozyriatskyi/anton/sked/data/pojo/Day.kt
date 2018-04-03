71a877d443fb0851746fbd29e5affe5cfdd27777
Anton Kozyriatskyi <dev.anton.kozyriatskyi@gmail.com>
2018-04-03 09:43:21 +0300

    Remove 'open' modifier

--- app/src/main/kotlin/kozyriatskyi/anton/sked/data/pojo/Day.kt
+++ app/src/main/kotlin/kozyriatskyi/anton/sked/data/pojo/Day.kt
@@ -5,18 +5,18 @@ import kozyriatskyi.anton.sked.data.repository.DateFormatter
 /**
  * Created by Anton on 22.08.2017.
  */
-open class Day(val dayNumber: Int, val weekNumber: Int, val date: String, val lessons: List<LessonDb>)
+class Day(val dayNumber: Int, val weekNumber: Int, val date: String, val lessons: List<LessonDb>)
 
-open class DayUi(val dayNum: Int, val weekNumber: Int, val day: String, val shortDate: String,
+class DayUi(val dayNum: Int, val weekNumber: Int, val day: String, val shortDate: String,
                  val lessons: List<LessonUi>)
 
-open class DayMapper(private val lessonMapper: LessonMapper, private val dateFormatter: DateFormatter) {
+class DayMapper(private val lessonMapper: LessonMapper, private val dateFormatter: DateFormatter) {
 
-    open fun dbToUi(day: Day): DayUi =
+    fun dbToUi(day: Day): DayUi =
             DayUi(day.dayNumber, day.weekNumber, dateFormatter.dayOfWeek(day.dayNumber),
                     dateFormatter.shortDate(day.date), lessonMapper.dbToView(day.lessons))
 
-    open fun dbToUi(days: List<Day>): List<DayUi> = days.map {
+    fun dbToUi(days: List<Day>): List<DayUi> = days.map {
         DayUi(it.dayNumber, it.weekNumber, dateFormatter.dayOfWeek(it.dayNumber),
                 dateFormatter.shortDate(it.date), lessonMapper.dbToView(it.lessons))
     }
