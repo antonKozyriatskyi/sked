af6ff796d2aef1833c2d4f32ddb2408416e675ad
Anton <dev.anton.kozyriatskyi@gmail.com>
2018-06-18 18:13:49 +0300

    Add audiences parser

---
+++ sut-parser/src/main/java/kozyriatskyi/anton/sutparser/ParsedAudience.kt
@@ -0,0 +1,6 @@
+package kozyriatskyi.anton.sutparser
+
+data class ParsedAudience(val number: String,
+                          val isFree: Boolean,
+                          val note: String,
+                          val capacity: String)
