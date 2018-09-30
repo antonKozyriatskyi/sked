10665db58a12f025bb4d3fbc70656ccce125b665
Anton <dev.anton.kozyriatskyi@gmail.com>
2018-09-30 13:21:13 +0300

    Add privacy policy

--- app/src/main/kotlin/kozyriatskyi/anton/sked/about/AboutActivity.kt
+++ app/src/main/kotlin/kozyriatskyi/anton/sked/about/AboutActivity.kt
@@ -10,12 +10,19 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
 import kozyriatskyi.anton.sked.BuildConfig
-import kozyriatskyi.anton.sked.R
 import kozyriatskyi.anton.sked.data.pojo.Library
 import java.util.*
 
 
 class AboutActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        private const val URL_PRIVACY_POLICY = "https://firebasestorage.googleapis.com/v0/b/sked-a797c.appspot.com/o/privacy_policy.html?alt=media&token=cf8d3456-a2ff-49c8-a9f6-f879e0e55775"
+        private const val URL_VK = "http://vk.com/kozyriatskyi"
+        private const val URL_TG_APP = "tg://resolve?domain=antonKozyriatskyi"
+        private const val URL_TG_WEB = "http://www.telegram.me/antonKozyriatskyi"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
@@ -28,26 +35,31 @@ class AboutActivity : AppCompatActivity(), View.OnClickListener {
                 BuildConfig.VERSION_NAME)
         findViewById<TextView>(R.id.about_tv_telegram).setOnClickListener(this)
         findViewById<TextView>(R.id.about_tv_vk).setOnClickListener(this)
+        findViewById<TextView>(R.id.about_tv_privacy_policy).setOnClickListener(this)
     }
 
 
     override fun onClick(view: View) {
         when (view.id) {
             R.id.about_tv_telegram -> {
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("tg://resolve?domain=antonKozyriatskyi"))
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(URL_TG_APP))
 
                 try {
                     startActivity(intent)
                 } catch (e: ActivityNotFoundException) {
-                    val i = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.telegram.me/antonKozyriatskyi"))
-                    startActivity(i)
+                    tryOpenBrowser(URL_TG_WEB)
                 }
 
             }
-            R.id.about_tv_vk -> {
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://vk.com/kozyriatskyi"))
-                startActivity(intent)
-            }
+            R.id.about_tv_vk -> tryOpenBrowser(URL_VK)
+            R.id.about_tv_privacy_policy -> tryOpenBrowser(URL_PRIVACY_POLICY)
+        }
+    }
+
+    private fun tryOpenBrowser(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
         }
     }
 
