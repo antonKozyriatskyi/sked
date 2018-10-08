652aa345e5e298dee8286025cb3f5f2601e5bda2
Anton <dev.anton.kozyriatskyi@gmail.com>
2018-10-08 20:29:27 +0300

    Add iconId property to AudienceUi

--- app/src/main/kotlin/kozyriatskyi/anton/sked/audiences/AudienceUi.kt
+++ app/src/main/kotlin/kozyriatskyi/anton/sked/audiences/AudienceUi.kt
@@ -1,10 +1,10 @@
 package kozyriatskyi.anton.sked.audiences
 
-import android.support.annotation.ColorRes
+import android.support.annotation.DrawableRes
 
 data class AudienceUi(val number: String,
                       val isFree: Boolean,
                       val status: String,
                       val note: String,
                       val capacity: String,
-                      @ColorRes val colorId: Int)
+                      @DrawableRes val iconId: Int)
