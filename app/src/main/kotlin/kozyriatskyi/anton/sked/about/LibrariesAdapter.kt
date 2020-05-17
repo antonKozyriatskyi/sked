329eea53355ccb0d5e86dcf9e87cd965c875c5ff
antonKozyriatskyi <akozyriatskyi.dev@gmail.com>
2020-05-17 22:54:23 +0300

    Remove long imports in LibrariesAdapter

--- app/src/main/kotlin/kozyriatskyi/anton/sked/about/LibrariesAdapter.kt
+++ app/src/main/kotlin/kozyriatskyi/anton/sked/about/LibrariesAdapter.kt
@@ -3,25 +3,26 @@ package kozyriatskyi.anton.sked.about
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import kozyriatskyi.anton.sked.R
 import kozyriatskyi.anton.sked.data.pojo.Library
 import kozyriatskyi.anton.sked.util.inflate
 
-class LibrariesAdapter(private val data: List<Library>) : androidx.recyclerview.widget.RecyclerView.Adapter<androidx.recyclerview.widget.RecyclerView.ViewHolder>() {
+class LibrariesAdapter(private val data: List<Library>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun getItemCount(): Int = data.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): androidx.recyclerview.widget.RecyclerView.ViewHolder =
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
             LibrariesHolder(parent.inflate(R.layout.item_library))
 
-    override fun onBindViewHolder(holder: androidx.recyclerview.widget.RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder is LibrariesHolder) {
             holder.name.text = data[position].name
             holder.license.text = data[position].license
         }
     }
 
-    private class LibrariesHolder(view: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(view) {
+    private class LibrariesHolder(view: View) : RecyclerView.ViewHolder(view) {
         var name: TextView = view.findViewById(R.id.library_name)
         var license: TextView = view.findViewById(R.id.library_license)
     }
