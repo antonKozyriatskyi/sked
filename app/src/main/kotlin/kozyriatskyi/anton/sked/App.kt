1004bf0f038c925ac9e9ea0a4be4632365160230
Anton Kozyriatskyi <akozyriatskyi.dev@gmail.com>
2021-11-30 21:11:52 +0200

    Remove relonching workmanager in App.onCreate

--- app/src/main/kotlin/kozyriatskyi/anton/sked/App.kt
+++ app/src/main/kotlin/kozyriatskyi/anton/sked/App.kt
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceManager
 import kozyriatskyi.anton.sked.data.repository.UserSettingsStorage
 import kozyriatskyi.anton.sked.di.Injector
-import kozyriatskyi.anton.sked.util.JobManager
-
 
 class App : BaseApplication() {
 
@@ -19,17 +17,10 @@ class App : BaseApplication() {
 
         PreferenceManager.setDefaultValues(this, R.xml.app_preferences, false)
 
-        relaunchUpdaterJob()
     }
-
 
     private fun applyTheme(sharedPreferences: UserSettingsStorage) {
         val defaultTheme = sharedPreferences.getString(UserSettingsStorage.KEY_DEFAULT_THEME, "0").toInt()
         AppCompatDelegate.setDefaultNightMode(defaultTheme)
-    }
-
-    // TODO Remove this in a next update
-    private fun relaunchUpdaterJob() {
-        JobManager(this).launchUpdaterJob()
     }
 }
