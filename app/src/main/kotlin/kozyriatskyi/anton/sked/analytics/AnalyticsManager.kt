d1647f8c3d5d23ae3a3f8323b324dc3ff065c80e
antonKozyriatskyi <akozyriatskyi.dev@gmail.com>
2022-09-04 13:03:56 +0300

    Remove incorrect files header

--- app/src/main/kotlin/kozyriatskyi/anton/sked/analytics/AnalyticsManager.kt
+++ app/src/main/kotlin/kozyriatskyi/anton/sked/analytics/AnalyticsManager.kt
@@ -1,8 +1,5 @@
 package kozyriatskyi.anton.sked.analytics
 
-/**
- * Created by Backbase R&D B.V. on 17.10.2021.
- */
 interface AnalyticsManager {
 
     fun logInfo(message: String)
--- app/src/main/kotlin/kozyriatskyi/anton/sked/common/BasePresenter.kt
+++ app/src/main/kotlin/kozyriatskyi/anton/sked/common/BasePresenter.kt
@@ -6,9 +6,6 @@ import kotlinx.coroutines.cancel
 import moxy.MvpPresenter
 import moxy.MvpView
 
-/**
- * Created by Backbase R&D B.V. on 11.10.2021.
- */
 abstract class BasePresenter<View: MvpView> : MvpPresenter<View>() {
 
     protected val scope = MainScope()
